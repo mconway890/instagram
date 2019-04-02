42618085b358b68e5313fde947c072abeab29552
Michele Conway <mconway890@gmail.com>
2019-04-02 15:36:24 -0400

    Update action, strong params and update route added for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,15 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def update
+    current_user.update(params[:user])
+    redirect_to current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :name, :website, :bio, :email, :phone, :gender)
+  end
 end
